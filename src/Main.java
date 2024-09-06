213e6739e97f84ce4bacdae517447028e407436f
barto3214 <bartek.slusarz@onet.pl>
2024-09-06 21:14:53 +0200

    Trzy funkcjonalności

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,102 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void jedynka(){
-        for(int i = 0;i<1002;i+=2){
+    public static void jedynka() {
+        for (int i = 0; i < 1002; i += 2) {
             System.out.println(i + " ");
         }
     }
 
-    public static String dwojka(String word){
+    public static String dwojka(String word) {
 
         return new StringBuilder(word).reverse().toString();
+    }
+
+    public static boolean trojka1(int number) {
+        String numb = Integer.toString(number);
+        String revnumb = new StringBuilder(numb).reverse().toString();
+        return numb.equals(revnumb);
+    }
+
+    public static int trojka2(int number) {
+        int higher = number + 1;
+        int lower = number - 1;
+        while (true) {
+            if (trojka1(higher)) {
+                return higher;
+            }
+            if (trojka1(lower)) {
+                return lower;
+            }
+
+            lower--;
+            higher++;
+        }
     }
 
     public static void main(String[] args) {
         Scanner klawiatura = new Scanner(System.in);
         String out;
-        do{
-        System.out.println("Wpisz jaką chcesz funkcjonalność (1-8)\n");
-        int decision = klawiatura.nextInt();
+        do {
+            System.out.println("Wpisz jaką chcesz funkcjonalność (1-8)\n");
+            int decision = klawiatura.nextInt();
             klawiatura.nextLine();
 
-        switch (decision){
-            case 1:
+            switch (decision) {
+                case 1:
                     jedynka();
-                break;
+                    break;
 
-            case 2:
-                System.out.println("Proszę wpisać słowo do sprawdzenia czy jest palindromem");
+                case 2:
+                    System.out.println("Proszę wpisać słowo do sprawdzenia czy jest palindromem");
 
-                String word = klawiatura.nextLine();
+                    String word = klawiatura.nextLine();
 
-                String reversed = dwojka(word);
+                    String reversed = dwojka(word);
 
-                if(word.equals(reversed)){
-                    System.out.println("To słowo jest palindromem");
-                }
-                else{
-                    System.out.println("To słowo nie jest palindromem");
-                }
-                break;
+                    if (word.equals(reversed)) {
+                        System.out.println("To słowo jest palindromem");
+                    } else {
+                        System.out.println("To słowo nie jest palindromem");
+                    }
+                    break;
 
-            case 3:
-                System.out.println("zrób coś1");
-                break;
+                case 3:
+                    System.out.println("Wprowadź liczbe do szukania w jej poliżu liczby palindromicznej");
+                    int number = klawiatura.nextInt();
+                    int closestpalindrom = trojka2(number);
+                    System.out.println("Najbliższy palindrom to " + closestpalindrom);
+                    klawiatura.nextLine();
+                    break;
 
-            case 4:
-                System.out.println("zrób coś2");
-                break;
+                case 4:
+                    System.out.println("zrób coś2");
+                    break;
 
-            case 5:
-                System.out.println("zrób coś3");
-                break;
+                case 5:
+                    System.out.println("zrób coś3");
+                    break;
 
-            case 6:
-                System.out.println("zrób coś4");
-                break;
+                case 6:
+                    System.out.println("zrób coś4");
+                    break;
 
-            case 7:
-                System.out.println("zrób coś5");
-                break;
+                case 7:
+                    System.out.println("zrób coś5");
+                    break;
 
-            case 8:
-                System.out.println("zrób coś");
-                break;
+                case 8:
+                    System.out.println("zrób coś");
+                    break;
 
-            default:
-                System.out.println("Widocznie jesteś idiotą bo nie umiesz wpisać");
-                break;
-        }
+                default:
+                    System.out.println("Widocznie jesteś idiotą bo nie umiesz wpisać");
+                    break;
+            }
+
             System.out.println("Chcesz zakończyć działanie?(Tak/Nie)");
             out = klawiatura.nextLine();
-    }while(out.equals("Nie"));
+        } while (out.equals("Nie"));
     }
 
 }
